2f6f208c6a017584ef86487e98c485f1acdb5c13
unknown <meenakshy25@gmail.com>
2022-10-08 12:43:22 +0530

    Bubble chart without external library

--- src/app/shared/bubble-chart/bubble-chart.component.ts
+++ src/app/shared/bubble-chart/bubble-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataset } from 'chart.js';
 import { getRandomRGB } from '../../utils';
 
@@ -7,54 +7,177 @@ import { getRandomRGB } from '../../utils';
   templateUrl: './bubble-chart.component.html',
   styleUrls: ['./bubble-chart.component.scss']
 })
-export class BubbleChartComponent implements OnChanges {
+export class BubbleChartComponent implements OnChanges, OnInit {
 
   constructor() { }
 
   @Input() chartData: any;
 
-  bubbleChartOptions: ChartOptions = {};
-  bubbleChartType: ChartType = 'bubble';
-  bubbleChartLegend: boolean = true;
-  bubbleChartFormatData:ChartDataset[] = [];
+  grid_size = 100;
+  grid_vertical_size = 50;
+  grid_horizontal_size = 200;
+  x_axis_starting_point = { number: 100, suffix: '' };
+  y_axis_starting_point = { number: 100, suffix: '' };
 
-  ngOnChanges(): void {
-       this.bubbleChartOptions = {
-        responsive: true,
-        scales: {
-          x: {
-            suggestedMin: 100,
-            suggestedMax: 200,
-            title: {
-              display: true,
-              text: 'Salary'
-            }
-          },
-          y: {
-              title: {
-                display: true,
-                text: 'Head Count'
-              },
-              suggestedMin: 100,
-              suggestedMax: 200,
-          }
-        },
-      };
-      this.bubbleChartType = 'bubble';
-      this.bubbleChartLegend = true;
-    
-      this.chartData.forEach((cData:any) => {
-        const { salary, compratio, headcount } = cData
+  canvas: any;
+  ctx: any;
+
+  canvas_width: any;
+  canvas_height: any;
+
+  num_lines_x: any;
+  num_lines_y: any;
+  x_axis_distance_grid_lines: any;
+  y_axis_distance_grid_lines: any;
+
+  ngOnChanges() {
+    console.log('chartData', this.chartData)
+    if (this.chartData.length > 0) {
+        this.chartData.forEach((data: any) => {
+        let x = (Math.floor(data.salary / 2))
+        console.log('data, x', data.salary, x)
+        let y = (Math.floor(data.headcount / 2))
+        console.log('data, y', data.headcount, y)
+        this.ctx?.beginPath();
+        this.ctx?.arc(data.salary, -data.headcount, data.compratio, 0, 2*Math.PI);
         const rgb = getRandomRGB();
-        const obj = {
-          data: [
-            { x: salary, y: headcount, r: compratio},
-          ],
-          title: "dataTitle3",
-          label: cData.title,
-          backgroundColor: ["rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ", 0.7)"]
-        }
-        this.bubbleChartFormatData.push(obj)
+        console.log('rgb', rgb)
+        this.ctx.fillStyle = "rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ", 0.5)";
+        this.ctx.fill();
+        this.ctx?.stroke();
+        this.ctx.beginPath();
+        this.ctx.fillStyle = "black";
+        this.ctx.textAlign = 'center';
+        this.ctx.font = "10px Arial";
+        this.ctx.fillText(data.title, data.salary, -data.headcount);
+        this.ctx.fill();
       })
+    }
+    // this.ctx?.beginPath();
+    // this.ctx?.arc(100, -100, 10, 0, 2*Math.PI);
+    // this.ctx?.stroke();
+
   }
+
+  ngOnInit() {
+    this.canvas = <HTMLCanvasElement> document.getElementById("myCanvas")
+    this.ctx = this.canvas.getContext("2d");
+
+    this.canvas_width = this.canvas.width;
+    this.canvas_height = this.canvas.height;
+
+    this.num_lines_x = Math.floor(this.canvas_height/this.grid_size);
+    this.num_lines_y = Math.floor(this.canvas_width/this.grid_size);
+
+    this.x_axis_distance_grid_lines = this.num_lines_x - 1;
+    this.y_axis_distance_grid_lines = 1;
+
+    // console.log('num_lines_x', num_lines_x)
+
+    // Draw grid lines along X-axis
+    for(let i=0; i<=this.num_lines_x; i++) {
+      this.ctx.beginPath();
+      this.ctx.lineWidth = 1;
+        
+        // If line represents X-axis draw in different color
+        if(i == this.x_axis_distance_grid_lines) 
+          this.ctx.strokeStyle = "#000000";
+        else
+          this.ctx.strokeStyle = "#e9e9e9";
+
+        this.ctx.moveTo(0, this.grid_size*i);
+        this.ctx.lineTo(this.canvas_width, this.grid_size*i);
+        this.ctx.stroke();
+    }
+
+    // Draw grid lines along Y-axis
+    for(let i=0; i<=this.num_lines_y; i++) {
+      this.ctx.beginPath();
+      this.ctx.lineWidth = 1;
+      
+      // If line represents X-axis draw in different color
+      if(i == this.y_axis_distance_grid_lines) 
+          this.ctx.strokeStyle = "#000000";
+      else
+          this.ctx.strokeStyle = "#e9e9e9";
+      
+      this.ctx.moveTo(this.grid_size*i, 0);
+      this.ctx.lineTo(this.grid_size*i, this.canvas_height);
+      this.ctx.stroke();
+  }
+  
+  // // Translate to the new origin. Now Y-axis of the canvas is opposite to the Y-axis of the graph. So the y-coordinate of each element will be negative of the actual
+  this.ctx.translate(this.y_axis_distance_grid_lines*this.grid_size, this.x_axis_distance_grid_lines*this.grid_size);
+  
+  // Ticks marks along the positive X-axis
+  for(let i=1; i<(this.num_lines_y - this.y_axis_distance_grid_lines); i++) {
+      this.ctx.beginPath();
+      this.ctx.lineWidth = 1;
+      this.ctx.strokeStyle = "#000000";
+  
+      // Draw a tick mark 6px long (-3 to 3)
+      this.ctx.moveTo(this.grid_size*i+0.5, -3);
+      this.ctx.lineTo(this.grid_size*i+0.5, 3);
+      this.ctx.stroke();
+  
+      // Text value at that point
+      this.ctx.font = '9px Arial';
+      this.ctx.textAlign = 'start';
+      this.ctx.fillText(this.x_axis_starting_point.number*i + this.x_axis_starting_point.suffix, this.grid_size*i-2, 15);
+  }
+  
+  // Ticks marks along the negative X-axis
+  for(let i=1; i<this.y_axis_distance_grid_lines; i++) {
+      this.ctx.beginPath();
+      this.ctx.lineWidth = 1;
+      this.ctx.strokeStyle = "#000000";
+  
+      // Draw a tick mark 6px long (-3 to 3)
+      this.ctx.moveTo(-this.grid_size*i+0.5, -3);
+      this.ctx.lineTo(-this.grid_size*i+0.5, 3);
+      this.ctx.stroke();
+  
+      // Text value at that point
+      this.ctx.font = '9px Arial';
+      this.ctx.textAlign = 'end';
+      this.ctx.fillText(-this.x_axis_starting_point.number*i + this.x_axis_starting_point.suffix, -this.grid_size*i+3, 15);
+  }
+  
+  // Ticks marks along the positive Y-axis
+  // Positive Y-axis of graph is negative Y-axis of the canvas
+  for(let i=1; i<(this.num_lines_x - this.x_axis_distance_grid_lines); i++) {
+      this.ctx.beginPath();
+      this.ctx.lineWidth = 1;
+      this.ctx.strokeStyle = "#000000";
+  
+      // Draw a tick mark 6px long (-3 to 3)
+      this.ctx.moveTo(-3, this.grid_size*i+0.5);
+      this.ctx.lineTo(3, this.grid_size*i+0.5);
+      this.ctx.stroke();
+  
+      // Text value at that point
+      this.ctx.font = '9px Arial';
+      this.ctx.textAlign = 'start';
+      this.ctx.fillText(-this.y_axis_starting_point.number*i + this.y_axis_starting_point.suffix, 8, this.grid_size*i+3);
+  }
+  
+  // // Ticks marks along the negative Y-axis
+  // Negative Y-axis of graph is positive Y-axis of the canvas
+  for(let i=1; i<this.x_axis_distance_grid_lines; i++) {
+      this.ctx.beginPath();
+      this.ctx.lineWidth = 1;
+      this.ctx.strokeStyle = "#000000";
+  
+      // Draw a tick mark 6px long (-3 to 3)
+      this.ctx.moveTo(-3, -this.grid_size*i+0.5);
+      this.ctx.lineTo(3, -this.grid_size*i+0.5);
+      this.ctx.stroke();
+  
+      // Text value at that point
+      this.ctx.font = '9px Arial';
+      this.ctx.textAlign = 'start';
+      this.ctx.fillText(this.y_axis_starting_point.number*i + this.y_axis_starting_point.suffix, 8, -this.grid_size*i+3);
+    }
+  }
+
 }
