d9a96cf3e077cb92a939aff89b4a7489728101b1
unknown <meenakshy25@gmail.com>
2022-10-12 12:07:26 +0530

    Formatted code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,50 @@
-import { Component } from '@angular/core';
-import { ChartDataService } from './services/chart-data.service';
+import { Component } from "@angular/core";
+import { ChartDataService } from "./services/chart-data.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  
   chartData: any;
- 
+
   constructor(public chartDataService: ChartDataService) {}
 
   ngOnInit() {
-    this.chartData = this.chartDataService.getChartData().subscribe((data: any) => {
-      this.chartData = data;
-    })
-  //   this.chartData = [
-  //     {
-  //        "title":"HR",
-  //        "salary":550,
-  //        "compratio":10,
-  //        "headcount":300
-  //     }
-  //     ,
-  //     {
-  //        "title":"Marketing",
-  //        "salary":660,
-  //        "compratio":25,
-  //        "headcount":380
-  //     },
-  //     {
-  //        "title":"Engineering",
-  //        "salary":700,
-  //        "compratio":30,
-  //        "headcount":550
-  //     },
-  //     {
-  //        "title":"Operations",
-  //        "salary":750,
-  //        "compratio":20,
-  //        "headcount":700
-  //     }
-  //  ];
+    this.chartData = this.chartDataService
+      .getChartData()
+      .subscribe((data: any) => {
+        this.chartData = data;
+      });
+    // To test the app using this static data, please un-comment lines in file bubble-chart.components.ts file 
+    // from line 47 to 50
+    //   this.chartData = [
+    //     {
+    //        "title":"HR",
+    //        "salary":550,
+    //        "compratio":10,
+    //        "headcount":300
+    //     }
+    //     ,
+    //     {
+    //        "title":"Marketing",
+    //        "salary":660,
+    //        "compratio":25,
+    //        "headcount":380
+    //     },
+    //     {
+    //        "title":"Engineering",
+    //        "salary":700,
+    //        "compratio":30,
+    //        "headcount":550
+    //     },
+    //     {
+    //        "title":"Operations",
+    //        "salary":750,
+    //        "compratio":20,
+    //        "headcount":700
+    //     }
+    //  ];
   }
-
 }
--- src/app/shared/bubble-chart/bubble-chart.component.ts
+++ src/app/shared/bubble-chart/bubble-chart.component.ts
@@ -1,14 +1,13 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { getRandomRGB } from '../../utils';
+import { Component, Input, OnChanges, OnInit } from "@angular/core";
+import { getRandomRGB } from "../../utils";
 
 @Component({
-  selector: 'app-bubble-chart',
-  templateUrl: './bubble-chart.component.html',
-  styleUrls: ['./bubble-chart.component.scss']
+  selector: "app-bubble-chart",
+  templateUrl: "./bubble-chart.component.html",
+  styleUrls: ["./bubble-chart.component.scss"],
 })
 export class BubbleChartComponent implements OnChanges, OnInit {
-
-  constructor() { }
+  constructor() {}
 
   @Input() chartData: any;
 
@@ -36,9 +35,19 @@ export class BubbleChartComponent implements OnChanges, OnInit {
   widthDiffX: any;
   widthDiffY: any;
 
-  ngOnChanges() {
-    if (this.chartData.length > 0) {
+  ngOnInit() {
+    this.canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
+    this.context = this.canvas.getContext("2d");
+    this.canvasWidth = this.canvas.width;
+    this.canvasHeight = this.canvas.height;
+  }
 
+  ngOnChanges() {
+    // this.canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
+    // this.context = this.canvas.getContext("2d");
+    // this.canvasWidth = this.canvas.width;
+    // this.canvasHeight = this.canvas.height;
+    if (this.chartData.length > 0) {
       this.findMaxNumOfLines(this.chartData);
 
       this.numOfLinesX = this.maxNumOfLinesX + 2;
@@ -51,115 +60,131 @@ export class BubbleChartComponent implements OnChanges, OnInit {
       this.yAxis = 1;
 
       // Draw grid lines along X-axis
-      for(let i=1; i<=this.numOfLinesX; i++) {
-        // this.context.beginPath();
+      for (let i = 1; i <= this.numOfLinesX; i++) {
         // If line represents X-axis draw in different color
-        if(i == this.xAxis) 
-        {
+        if (i == this.xAxis) {
           this.context.strokeStyle = "#000000";
           // Label for X-Axis
-          this.fillText('Salary', '15px', (this.canvasWidth/2) + 50, this.widthBtwLinesX*i + 50);
-        }
-        else {
+          this.drawText(
+            "Salary",
+            "15px",
+            this.canvasWidth / 2 + 50,
+            this.widthBtwLinesX * i + 50
+          );
+        } else {
           this.context.strokeStyle = "#e9e9e9";
         }
-        this.drawLine(0, this.widthBtwLinesX*i, this.canvasWidth, this.widthBtwLinesX*i);
+        this.drawLine(
+          0,
+          this.widthBtwLinesX * i,
+          this.canvasWidth,
+          this.widthBtwLinesX * i
+        );
       }
 
       // Draw grid lines along Y-axis
-      for(let i=1; i<=this.numOfLinesY; i++) {
-        this.context.beginPath();
-        this.context.lineWidth = 1;
+      for (let i = 1; i <= this.numOfLinesY; i++) {
         // If line represents X-axis draw in different color
-        if(i == this.yAxis) 
-        {
-            this.context.strokeStyle = "#000000";
-            // Label for X-Axis
-            this.context.save();
-            this.context.rotate(-0.5*Math.PI);
-            this.fillText('Head Count', '15px', -250, 100);
-            this.context.restore();
+        if (i == this.yAxis) {
+          this.context.strokeStyle = "#000000";
+          // Label for X-Axis
+          this.context.save();
+          this.context.rotate(-0.5 * Math.PI);
+          this.drawText("Head Count", "15px", -250, 100);
+          this.context.restore();
+        } else {
+          this.context.strokeStyle = "#e9e9e9";
         }
-        else {
-            this.context.strokeStyle = "#e9e9e9";
-        }
-        this.drawLine(this.widthBtwLinesY*i, 0, this.widthBtwLinesY*i, this.canvasHeight);
-    }
-    
-    // // Translate to the new origin. Now Y-axis of the canvas is opposite to the Y-axis of the graph. So the y-coordinate of each element will be negative of the actual
-    this.context.translate(this.yAxis*this.widthBtwLinesY, this.xAxis*this.widthBtwLinesX);
+        this.drawLine(
+          this.widthBtwLinesY * i,
+          0,
+          this.widthBtwLinesY * i,
+          this.canvasHeight
+        );
+      }
 
-    // Tick marks along x-axis
-    for (let i=1; i<=this.maxNumOfLinesY; i++) {
-      this.context.beginPath();
-      this.context.lineWidth = 1;
-      this.context.strokeStyle = "#000000";
-      this.drawLine((this.widthBtwLinesY*(i)), -3, (this.widthBtwLinesY*(i)), +3)
-      this.fillText(this.xAxisStartingPoint + ((i-1)*100), '9px', this.widthBtwLinesY*i-2, 15)
-    }
-    
-    // Tick marks along the Y-axis
-    for(let i=1; i<=this.maxNumOfLinesX; i++) {
-      this.context.beginPath();
-      this.context.lineWidth = 1;
-      this.context.strokeStyle = "#000000";
+      // // Translate to the new origin. Now Y-axis of the canvas is opposite to the Y-axis of the graph. So the y-coordinate of each element will be negative of the actual
+      this.context.translate(
+        this.yAxis * this.widthBtwLinesY,
+        this.xAxis * this.widthBtwLinesX
+      );
 
-      this.drawLine(-3, -this.widthBtwLinesX*i, 3, -this.widthBtwLinesX*i)
-      this.fillText(this.yAxisStartingPoint + ((i-1)*100), '9px', -20, -this.widthBtwLinesX*i+3)
-    }
+      // Tick marks along x-axis
+      for (let i = 1; i <= this.maxNumOfLinesY; i++) {
+        this.context.strokeStyle = "#000000";
+        this.drawLine(this.widthBtwLinesY * i, -3, this.widthBtwLinesY * i, +3);
+        this.drawText(
+          this.xAxisStartingPoint + (i - 1) * 100,
+          "9px",
+          this.widthBtwLinesY * i - 2,
+          15
+        );
+      }
+
+      // Tick marks along the Y-axis
+      for (let i = 1; i <= this.maxNumOfLinesX; i++) {
+        this.context.strokeStyle = "#000000";
+        this.drawLine(
+          -3,
+          -this.widthBtwLinesX * i,
+          3,
+          -this.widthBtwLinesX * i
+        );
+        this.drawText(
+          this.yAxisStartingPoint + (i - 1) * 100,
+          "9px",
+          -20,
+          -this.widthBtwLinesX * i + 3
+        );
+      }
 
       this.widthDiffX = (this.widthBtwLinesY - 100) / 100;
-      this.widthDiffY = (this.widthBtwLinesX - 100) / 100;  
+      this.widthDiffY = (this.widthBtwLinesX - 100) / 100;
       this.chartData.forEach((data: any) => {
         this.context?.beginPath();
 
         let { salary, compratio, headcount } = data;
 
-        salary = ((salary - this.xAxisStartingPoint) + 100)
-        salary = salary + (salary * this.widthDiffX);
-        headcount = ((headcount - this.yAxisStartingPoint) + 100)
-        headcount = headcount + (headcount * this.widthDiffY);
-        
-        this.context?.arc(salary, -headcount, compratio, 0, 2*Math.PI);
-        
+        salary = salary - this.xAxisStartingPoint + 100;
+        salary = salary + salary * this.widthDiffX;
+        headcount = headcount - this.yAxisStartingPoint + 100;
+        headcount = headcount + headcount * this.widthDiffY;
+
+        this.context?.arc(salary, -headcount, compratio, 0, 2 * Math.PI);
+
         const rgb = getRandomRGB();
-        this.context.fillStyle = "rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ", 0.5)";
+        this.context.fillStyle =
+          "rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ", 0.5)";
         this.context.fill();
         this.context?.stroke();
 
-        this.fillText(data.title, '10px', salary, -headcount)
-      })
+        this.drawText(data.title, "10px", salary, -headcount);
+      });
     }
-  }
-
-  ngOnInit() {
-    this.canvas = <HTMLCanvasElement> document.getElementById("myCanvas")
-    this.context = this.canvas.getContext("2d");
-    this.canvasWidth = this.canvas.width;
-    this.canvasHeight = this.canvas.height;
   }
 
   findMaxNumOfLines(chartData: any) {
     let minX, maxX;
     let minY, maxY;
-    let arrX: any = [], arrY: any = [];
+    let arrX: any = [],
+      arrY: any = [];
 
     chartData.forEach((data: any) => {
       arrX.push(data.salary);
       arrY.push(data.headcount);
-    })
+    });
 
     minX = Math.min(...arrX);
     maxX = Math.max(...arrX);
     minY = Math.min(...arrY);
     maxY = Math.max(...arrY);
 
-    minX = Math.floor(minX/100)*100
-    maxX = Math.ceil(maxX/100)*100
+    minX = Math.floor(minX / 100) * 100;
+    maxX = Math.ceil(maxX / 100) * 100;
 
-    minY = Math.floor(minY/100)*100
-    maxY = Math.ceil(maxY/100)*100
-    
+    minY = Math.floor(minY / 100) * 100;
+    maxY = Math.ceil(maxY / 100) * 100;
+
     // Graph starts with 100
     if (minX < 100) {
       minX = 100;
@@ -172,24 +197,24 @@ export class BubbleChartComponent implements OnChanges, OnInit {
     this.xAxisStartingPoint = minX;
     this.yAxisStartingPoint = minY;
 
-    this.maxNumOfLinesY = ((maxX/100) - (minX/100)) + 2;
-    this.maxNumOfLinesX = ((maxY/100) - (minY/100)) + 2;
+    this.maxNumOfLinesY = maxX / 100 - minX / 100 + 2;
+    this.maxNumOfLinesX = maxY / 100 - minY / 100 + 2;
   }
 
   drawLine(x1: any, y1: any, x2: any, y2: any) {
+    this.context.beginPath();
+    this.context.lineWidth = 1;
     this.context.moveTo(x1, y1);
     this.context.lineTo(x2, y2);
     this.context.stroke();
   }
 
-  fillText(text: string, fontSize: any, x: any, y: any) {
+  drawText(text: string, fontSize: any, x: any, y: any) {
     this.context.beginPath();
     this.context.fillStyle = "black";
-    this.context.textAlign = 'center';
+    this.context.textAlign = "center";
     this.context.font = fontSize + " Arial";
     this.context.fillText(text, x, y);
     this.context.fill();
   }
-  
-
 }
