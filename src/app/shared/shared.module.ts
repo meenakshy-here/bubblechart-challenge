4a142ec586fa415a6a13ca1439956026c5be2dd8
unknown <meenakshy25@gmail.com>
2022-10-19 15:46:04 +0530

    Removal of chart.js reference

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
-import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     BubbleChartComponent
   ],
   imports: [
-    CommonModule,
-    NgChartsModule
+    CommonModule
   ],
   exports: [
     BubbleChartComponent
